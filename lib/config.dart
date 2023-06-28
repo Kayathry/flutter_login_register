a39ee5f5c3e2834fe1d4fc15167b0ec1889fb970
Kayathry <iamgayathrivijayakumar@gmail.com>
2023-06-29 01:19:54 +0530

    first commit

---
+++ lib/config.dart
@@ -0,0 +1,7 @@
+class Config {
+  static const String appName = "Shopping App";
+  static const String apiURL = '198.168.1.102'; //PROD_URL
+  static const loginAPI = "/users/login";
+  static const registerAPI = "/users/register";
+  static const userProfileAPI = "/users/user-Profile";
+}
